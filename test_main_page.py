b876f49180479bf87c2b2e401fe10a61f30a9a9a
vittosik <43990487+vittosik@users.noreply.github.com>
2020-04-26 11:17:33 +0300

    Add files via upload

---
+++ test_main_page.py
@@ -0,0 +1,6 @@
+import time
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link").click()
